2c0cddaab2999f88fdc0d15942855575ff83419e
fajrwajr <bookreaderfajr@gmail.com>
2021-10-10 18:20:29 +0000

    Randomized icon

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+let icons = ["♦", "♥", "♠", "♣"];
+
+let iconMaker = icons[Math.floor(Math.random() * icons.length)];
+document.querySelector("#cardTwo").innerHTML = iconMaker;
+
 function cardMaker() {
   let result = Math.floor(Math.random() * 4) + 1;
   document.querySelector("#card").innerHTML = result;
