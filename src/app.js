bd33a67724968c42bbafe4e0a50c12a23318608f
fajrwajr <bookreaderfajr@gmail.com>
2021-10-10 23:44:25 +0000

    Centered Content

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ let icons = ["♦", "♥", "♠", "♣"];
 let iconMaker = icons[Math.floor(Math.random() * icons.length)];
 document.querySelector("#cardTwo").innerHTML = iconMaker;
 
+document.querySelector("#cardTw1").innerHTML = iconMaker;
+
 let randomLetters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, "K", "Q", "J", "A"];
 
 function cardDisplay() {
