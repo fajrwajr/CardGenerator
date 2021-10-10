425728a99915b038f5639992469a754d486f4dc7
fajrwajr <bookreaderfajr@gmail.com>
2021-10-10 20:18:08 +0000

    Added Numbers to Array

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ let icons = ["♦", "♥", "♠", "♣"];
 let iconMaker = icons[Math.floor(Math.random() * icons.length)];
 document.querySelector("#cardTwo").innerHTML = iconMaker;
 
-function cardMaker() {
-  let result = Math.floor(Math.random() * 4) + 1;
+let randomLetters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, "K", "Q", "J", "A"];
+
+function cardDisplay() {
+  let result = randomLetters[Math.floor(Math.random() * randomLetters.length)];
   document.querySelector("#cardOne").innerHTML = result;
 }
 
 let myStyle = document.querySelector("div");
 myStyle.style.background = "white";
-cardMaker();
+cardDisplay();
