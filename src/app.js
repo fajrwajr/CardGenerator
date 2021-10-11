1e2567c9081be2dcaa13f275a0c36075742a3cf1
fajrwajr <bookreaderfajr@gmail.com>
2021-10-11 20:16:02 +0000

    Added Interval

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
-let icons = ["♦", "<i class='color'>♥</i>", "♠", "♣"];
-
-let iconMaker = icons[Math.floor(Math.random() * icons.length)];
-document.querySelector("#cardTwo").innerHTML = iconMaker;
-
-document.querySelector("#cardTw1").innerHTML = iconMaker;
-
-let randomLetters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, "K", "Q", "J", "A"];
-
-function cardDisplay() {
-  let result = randomLetters[Math.floor(Math.random() * randomLetters.length)];
-  document.querySelector("#cardOne").innerHTML = result;
-}
-
 let myStyle = document.querySelector("div");
 myStyle.style.background = "white";
+
+function cardDisplay() {
+  setInterval(function() {
+    let icons = ["♦", "<i class='color'>♥</i>", "♠", "♣"];
+
+    let iconMaker = icons[Math.floor(Math.random() * icons.length)];
+    document.querySelector("#cardTwo").innerHTML = iconMaker;
+
+    document.querySelector("#cardTw1").innerHTML = iconMaker;
+
+    let randomLetters = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      "K",
+      "Q",
+      "J",
+      "A"
+    ];
+
+    let result =
+      randomLetters[Math.floor(Math.random() * randomLetters.length)];
+    document.querySelector("#cardOne").innerHTML = result;
+  }, 3000);
+}
+
 cardDisplay();
+//document.querySelector("#change").addEventListener("click", cardDisplay);
